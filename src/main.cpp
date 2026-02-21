8ac09f2de99686f2e5394cd6a17ea2336d8f47d7
Ben <meowcat@meowcat.site>
2026-02-21 09:00:39 +0000

    should show the image in console

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <citro2d.h>
 int main() {
     gfxInitDefault();
+    consoleInit(GFX_BOTTOM, NULL);
     romfsInit();
     C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
     C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
@@ -22,9 +23,9 @@ int main() {
         if (hidKeysDown() & KEY_START) {
             break;
         }
-        C2D_SceneBegin(top);
         C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-        C2D_TargetClear(top, C2D_Color32(0,0,0,255));
+        C2D_TargetClear(top, C2D_Color32(0, 0, 0, 255));
+        C2D_SceneBegin(top);
         C2D_DrawSprite(&spr);
         C3D_FrameEnd(0);
     }
